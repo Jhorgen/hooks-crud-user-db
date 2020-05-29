dcd8441a251128274a7581f79d046be488c3246c
Josh <Joshua.horgen@gmail.com>
2020-05-28 23:01:08 -0700

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-center mb-5'>CRUD app + hooks</h1>
+      <div className='d-flex-row'>
+        <div className='d-flex justify-content-around mt-1'>
+          <h2>Add user</h2>
+          <h2>View users</h2>
+        </div>
+      </div>
     </div>
   );
 }
