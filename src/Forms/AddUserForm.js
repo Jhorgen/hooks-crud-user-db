410350cee34149131269769d1952866f9fa6dadb
Josh <Joshua.horgen@gmail.com>
2020-10-26 02:20:48 -0700

    Prep for adding state to addUserForm

--- src/Forms/AddUserForm.js
+++ src/Forms/AddUserForm.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react'
 
-const AddUserForm = props => (
-  <div>
-  <p>test</p>
-  </div>
+const AddUserForm = (props) => (
+return (
+    <form>
+      <label>Name</label>
+      <input type='text' name='name'></input>
+      <label>Username</label>
+      <input type='text' name='username'></input>
+      <button>Add new user</button>
+    </form>
+ )
 )
 
 export default AddUserForm;
